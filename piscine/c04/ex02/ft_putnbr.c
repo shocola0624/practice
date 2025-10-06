709037780b6ca58d55aa415c5462220e2b85be60
shocola <sho_kotera@yahoo.co.jp>
2025-10-06 16:32:15 +0900

    putnbr

--- piscine/c04/ex02/ft_putnbr.c
+++ piscine/c04/ex02/ft_putnbr.c
@@ -2,7 +2,9 @@
 
 int abs(int nb)
 {
-    
+    if (nb < 0)
+        nb *= -1;
+    return (nb);
 }
 
 void    print_nbr(int nb)
@@ -10,7 +12,14 @@ void    print_nbr(int nb)
     char    n;
 
     if (nb >= 10)
-        print_nbr(nb / 10);
-    n = nb % 10 + '0';
+        print_nbr(abs(nb / 10));
+    n = abs(nb % 10) + '0';
     write (1, &n, 1);
+}
+
+void    ft_putnbr(int nb)
+{
+    if (nb < 0)
+        write (1, "-", 1);
+    print_nbr(nb);
 }
