e39c9428aaccabdbce48f7fd8a4d079247d5b836
shocola <sho_kotera@yahoo.co.jp>
2025-10-09 12:23:09 +0900

    c12/ex08

---
+++ piscine/c12/ex08/ft_list_reverse.c
@@ -0,0 +1,20 @@
+#include "ft_list.h"
+#include <stddef.h>
+
+void    ft_list_reverse(t_list **begin_list)
+{
+    t_list  *prev;
+    t_list  *cur;
+    t_list  *next;
+
+    prev = NULL;
+    cur = *begin_list;
+    while (cur)
+    {
+        next = cur->next;
+        cur->next = prev;
+        prev = cur;
+        cur = next;
+    }
+    *begin_list = prev;
+}
