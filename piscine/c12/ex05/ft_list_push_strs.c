d25634a94fbb6001f25313afa11d8f656cc1faa3
shocola <sho_kotera@yahoo.co.jp>
2025-10-09 12:22:50 +0900

    c12/ex05

--- piscine/c12/ex05/ft_list_push_strs.c
+++ piscine/c12/ex05/ft_list_push_strs.c
@@ -1,6 +1,21 @@
 #include "ft_list.h"
+#include <stddef.h>
 
 t_list  *ft_list_push_strs(int size, char **strs)
 {
-    
+    int     i;
+    t_list  *list;
+    t_list  *head;
+
+    if (!strs || size <= 0)
+        return (NULL);
+    i = 0;
+    head = NULL;
+    while (i < size)
+    {
+        list = ft_create_elem(strs[i++]);
+        list->next = head;
+        head = list;
+    }
+    return (head);
 }
