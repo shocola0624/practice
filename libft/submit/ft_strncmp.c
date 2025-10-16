c3cbf55f6d8cc2295dd4958e866806a62008f6df
shocola <sho_kotera@yahoo.co.jp>
2025-10-16 19:43:51 +0900

    libft/strncmp

---
+++ libft/submit/ft_strncmp.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncmp.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: skotera <skotera@student.42tokyo.jp>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/16 19:35:31 by skotera           #+#    #+#             */
+/*   Updated: 2025/10/16 19:35:31 by skotera          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int ft_strncmp(const char *s1, const char *s2, unsigned int n)
+{
+    int i;
+
+    i = 0;
+    if (n == 0)
+        return (0);
+    while (s1[i] && s2[i] && i < n - 1)
+    {
+        if ((unsigned char)s1[i] != (unsigned char)s2[i])
+            return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+        i++;
+    }
+    return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+}
