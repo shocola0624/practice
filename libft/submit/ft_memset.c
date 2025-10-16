c8725eb8581eab8a1392cf8b0aa21d0c35241300
shocola <sho_kotera@yahoo.co.jp>
2025-10-16 14:37:29 +0900

    libft/memset

---
+++ libft/submit/ft_memset.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memset.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: skotera <skotera@student.42tokyo.jp>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/16 11:35:49 by skotera           #+#    #+#             */
+/*   Updated: 2025/10/16 11:35:49 by skotera          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void    *ft_memset(void *s, int c, unsigned int n)
+{
+    unsigned char *p;
+    unsigned char v;
+
+    p = (unsigned char *)s;
+    v = (unsigned char)c;
+    while (n--)
+        *p++ = v;
+    return (s);
+}
