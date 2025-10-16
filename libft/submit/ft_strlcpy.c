5c39a779e24b8421a567b19bce10cfb370d2afdb
shocola <sho_kotera@yahoo.co.jp>
2025-10-16 18:28:16 +0900

    libft/strlcpy

---
+++ libft/submit/ft_strlcpy.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: skotera <skotera@student.42tokyo.jp>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/16 17:58:07 by skotera           #+#    #+#             */
+/*   Updated: 2025/10/16 17:58:07 by skotera          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+unsigned int    ft_strlcpy(char *restrict dest, const char *restrict src,
+    unsigned int n)
+{
+    unsigned int    i;
+
+    i = 0;
+    if (n > 0)
+    {
+        while (i < n - 1 && src[i])
+        {
+            dest[i] = src[i];
+            i++;
+        }
+        dest[i] = '\0';
+    }
+    while (src[i])
+        i++;
+    return (i);
+}
